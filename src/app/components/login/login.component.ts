bf68f34f1ce94d036a5cf134ede79c02cd143c29
foxx08 <samsepiol08@protonmail.com>
2021-11-23 11:18:51 +0100

    added register and login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public username: string = '';
+  public password: string = '';
+  public failed: boolean = false;
+  
+
+  public constructor(private backendService: BackendService, private router: Router) {}
+
+  public performAuthentification(): void {
+    this.backendService.login(this.username, this.password).then(result=> {
+      //console.log(result);
+      if(result) {
+        this.router.navigate(["/friends"]);
+      } else {
+        this.failed = true;
+      }
+    })
+  }
+
+  public ngOnInit(): void {}
+}
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  public username: string = '';
+  public password: string = '';
+  public confirm: string = '';
+  public usernameOk: boolean = false;
+  public passwordOk: boolean = false;
+  public userExist: boolean = false;
+  
+
+  public constructor(private backendService: BackendService, private router: Router) {}
+
+  public ngOnInit(): void {}
+
+  public userExists(): void {
+    this.backendService.userExists(this.username).then(result=> {
+      this.userExist = result;
+      if(this.userExist=result) {
+        this.usernameOk = false;
+      } else {
+        this.usernameOk = true;
+      }
+    })
+  }
+
+  public registerUser(): void {
+    this.backendService.register(this.username, this.password);
+    this.router.navigate(["/friends"]);
+  }
+
+  public checkUsername(): void {
+    if (this.username.length < 3) {
+      this.usernameOk = false;
+    } else {
+      this.usernameOk = true;
+    }
+  }
+
+  public checkPassword(): void {
+    if (this.password.length < 8) {
+      this.passwordOk = false;
+    } else {
+      this.passwordOk = true;
+    }
+  }
+
+  public checkConfirm(): void {
+    if (this.password != this.confirm) {
+      this.passwordOk = false;
+    } else {
+      this.passwordOk = true;
+    }
+  }
+
+  //submit or not
+  public createAccount(): void {
+    this.checkUsername();
+    this.checkPassword();
+    this.checkConfirm();
+    this.userExists();
+  }
+}
